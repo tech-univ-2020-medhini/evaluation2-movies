479bf82d4d9db65c5f22ab0e5e7fab150852e2e3
medhinioak <medhinioak@gmail.com>
2020-02-04 12:46:14 +0530

    Added error cases for getDetails

--- src/handlers/getDetails.js
+++ src/handlers/getDetails.js
@@ -5,7 +5,9 @@ const getDetails = async(request, h) => {
 	try{
 		const id = request.params.id;
 		const response = await dbOperations.getMovie(id);
-		console.log(response);
+		if(!response){
+			return h.response('No data found').code(204);
+		}
 		return h.response(response).code(200);
 	} catch(err){
 		return h.response(err.message).code(500);
--- tests/handlers/getDetails.test.js
+++ tests/handlers/getDetails.test.js
@@ -35,5 +35,71 @@ describe('The get deatails handler', () => {
 		expect(mockCode).toHaveBeenCalledWith(200);
 		mockDetails.mockRestore();
 	});
+	it ('Should return 500 if the db operation fails', async() => {
+		const mockReq = {
+			params: {
+				id : 1
+			},
+		};
+		const mockCode = jest.fn();
+		const mockH = {
+			response: jest.fn(()=>{
+				return {code: mockCode};
+			}),
+		};
+		const resolvedValue = {
+			'name': 'Seven',
+			'genres': [
+				'Mystery',
+				'Thriller',
+				'Crime'
+			],
+			'actors': [
+				'Brad Pitt',
+				'Morgan Freeman'
+			]
+		};
+		const mockDetails= jest.spyOn(dbOperation, 'getMovie');
+		mockDetails.mockRejectedValue(new Error('Internal server error'));
+		await getDetails(mockReq, mockH);
+        
+		expect(mockDetails).toHaveBeenCalledWith(mockReq.params.id);
+		expect(mockH.response).toHaveBeenCalledWith('Internal server error');
+		expect(mockCode).toHaveBeenCalledWith(500);
+		mockDetails.mockRestore();
+	});
+	it ('Should return 204 no data if the id is not found', async() => {
+		const mockReq = {
+			params: {
+				id : 1
+			},
+		};
+		const mockCode = jest.fn();
+		const mockH = {
+			response: jest.fn(()=>{
+				return {code: mockCode};
+			}),
+		};
+		const resolvedValue = {
+			'name': 'Seven',
+			'genres': [
+				'Mystery',
+				'Thriller',
+				'Crime'
+			],
+			'actors': [
+				'Brad Pitt',
+				'Morgan Freeman'
+			]
+		};
+		const mockDetails= jest.spyOn(dbOperation, 'getMovie');
+		mockDetails.mockResolvedValue(null);
+		await getDetails(mockReq, mockH);
+        
+		expect(mockDetails).toHaveBeenCalledWith(mockReq.params.id);
+		expect(mockH.response).toHaveBeenCalledWith('No data found');
+		expect(mockCode).toHaveBeenCalledWith(204);
+		mockDetails.mockRestore();
+	});
 
 });
