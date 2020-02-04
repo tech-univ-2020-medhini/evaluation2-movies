30c15c0fea4e54d619bcf38a84d7363935c96e57
medhinioak <medhinioak@gmail.com>
2020-02-04 10:32:56 +0530

    Added insertGenre Helper

--- src/helpers/dbOperations.js
+++ src/helpers/dbOperations.js
@@ -1,11 +1,26 @@
 const db = require('../../models/index');
 
-const insertMovies = async(moviesJson) => {
+const insertMovies = async(moviesJson, genresJson) => {
 	const moviesArray = moviesJson.movies;
-	moviesArray.forEach(element => {
+	moviesArray.forEach(async(element) => {
 		const jsonAdd = {moviesid:element.id,moviesname:element.name};
-		db.Movies.create(jsonAdd);
+		const result = await db.Movies.create(jsonAdd);
+		await insertGenres(genresJson, result, element.genres);
+		console.log(result);
 	});    
 };
 
-module.exports = {insertMovies};
+const insertGenres = async(genresJson, model, genreArray) => {
+	const genresApiArray = genresJson.genres;
+	genreArray.forEach(async(element) => {
+		genresApiArray.forEach(async(objArrayElement) => {
+			if (objArrayElement.id === element){
+				model.genre.push(objArrayElement.name);
+			}
+		});
+	});
+	await model.save();
+    
+};
+
+module.exports = {insertMovies, insertGenres};
--- tests/helpers/dbOperations.test.js
+++ tests/helpers/dbOperations.test.js
@@ -1,30 +1,40 @@
-const {insertMovies} = require('../../src/helpers/dbOperations');
+const dbOps = require('../../src/helpers/dbOperations');
 const db  = require('../../models/index');
+
+
 describe('The db operations', () => {
+	const jsonMovies = {'movies':[{'id':'6638453965','name':'The Shawshank Redemption','genres':[2,4]}]};
+	const jsonGenres = {'genres':[{'name':'Crime','id':1},{'name':'Mystery','id':2},{'name':'Thriller','id':3},{'name':'Romance','id':4},{'name':'Drama','id':5},{'name':'Sci-fi','id':6}]};
+	const jsonActors = {'actors':[{'name':'Brad Pitt','movies':['7533474498','1393797017','6621531523']}]};
 	describe('The insert movies function',() => {
 		it('Should add movieid and moviename fetched from the api into the db', async() => {
-			const jsonObj = {'movies':[{'id':'6638453965','name':'The Shawshank Redemption','genres':[2,4]}]};
+			//const jsonGenres = {'genres':[{'name':'Crime','id':1},{'name':'Mystery','id':2},{'name':'Thriller','id':3},{'name':'Romance','id':4},{'name':'Drama','id':5},{'name':'Sci-fi','id':6}]};
 			const mockDb = jest.spyOn(db.Movies, 'create');
 			mockDb.mockResolvedValue(true);
-			await insertMovies(jsonObj);
+			//dbOps.insertGenres = jest.fn();
+			await dbOps.insertMovies(jsonMovies,jsonGenres);
 			expect(mockDb).toHaveBeenCalledWith({'moviesid':'6638453965','moviesname':'The Shawshank Redemption'});
+			//expect(dbOps.insertGenres).toHaveBeenCalled();
 			mockDb.mockRestore();
+			//dbOps.insertGenres.mockReset();
 		});
 	});
-	xdescribe('The insert genres function',() => {
+	describe('The insert genres function',() => {
 		it('Should add genres fetched from the api into the db', async() => {
-			const mockDb = jest.spyOn(db.Movies, 'create');
-			mockDb.mockResolvedValue(true);
-			await insertGenres();
-			mockDb.mockRestore();
+			const mockModel = {
+				save : jest.fn()
+			};
+			await dbOps.insertGenres(jsonGenres,mockModel,[2,4]);
+			expect(mockModel.save).toHaveBeenCalled();
 		});
 	});
 	xdescribe('The insert actors function',() => {
 		it('Should add actors fetched from the api into the db', async() => {
-			const mockDb = jest.spyOn(db.Movies, 'create');
-			mockDb.mockResolvedValue(true);
+			const mockModel = {
+				save : jest.fn()
+			};
 			await insertActors();
-			mockDb.mockRestore();
+			mockModel.save.mockReset();
 		});
 	});
 });
